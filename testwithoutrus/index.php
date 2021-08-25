7bfa9f9189fb3c51dcfcf547e3750b323b9b11d6
daniilboyko <dboyko@novapulsar.com>
2021-08-25 16:39:38 +0300

    framework-yii 1.1 using docker container

---
+++ testwithoutrus/index.php
@@ -0,0 +1,15 @@
+<?php
+
+//testing xdebug configuration
+//phpinfo ();
+// die;
+
+// change the following paths if necessary
+$yii=dirname(__FILE__).'/../yii/framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
+
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once($yii);
+Yii::createWebApplication($config)->run();
